296cf77ac3724f69f808e635bb33480cba6e0def
silentp84 <silentp84@hotmail.com>
2018-07-23 12:54:18 -0500

    Beginner stuff
    I can do basic stuff with OOP

---
+++ CS_Basic 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 12 * 30;
+      Console.WriteLine(x);
+      string z = Console.ReadLine();
+      int y = Int32.Parse(z);
+      Console.WriteLine("Converting " + y + " feet to inches...");
+      Console.WriteLine("Feet to inches: "+feet_to_inches(y));
+    }
+    static int feet_to_inches(int feet)
+    {
+      return feet * 12;
+    }
+  }
+}
