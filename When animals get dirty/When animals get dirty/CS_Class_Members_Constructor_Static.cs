9fbd01570f3719e673a8fb6b321aebb47988114a
silentp84 <silentp84@hotmail.com>
2018-09-08 22:49:58 -0500

    Jon helps
    Files added to project

---
+++ When animals get dirty/When animals get dirty/CS_Class_Members_Constructor_Static.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+  public class Emos
+  {
+    public string Name;
+    public static int Scars;
+
+    public Emos(string n)
+    {
+      Name = n;
+      Scars++;
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Emos ruggle = new Emos("rlly?");
+      Console.WriteLine(ruggle.Name + " " + Emos.Scars);
+      Emos todd = new Emos("todd");
+      Console.WriteLine(todd.Name + " " + Emos.Scars);
+      todd = new Emos("nodd");
+      Console.WriteLine(todd.Name + " " + Emos.Scars);
+    }
+  }
+}
---
+++ When animals get dirty/When animals get dirty/CS_Ref Modifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+  class Program
+  {
+    static void Swap(ref string a, ref string b)
+    {
+      string temp = a; //all these assignments are by value and not reference
+      a = b;
+      b = temp;
+    }
+    static void Main(string[] args)
+    {
+      string x = "Penn";
+      string y = "Teller";
+      Swap(ref x, ref y);
+      Console.WriteLine(x);
+      Console.WriteLine(y);
+    }
+  }
+}
